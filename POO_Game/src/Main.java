f67fd8b823ff1a5d26443aab3939b1db207b97de
Milagros200118 <124943781+Milagros200118@users.noreply.github.com>
2023-02-09 21:10:50 -0500

    EDITADO POR MILAGROS LLAUCE

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+//EDITADO POR MILAGROS LLAUCE
 
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
